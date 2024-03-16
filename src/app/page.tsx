9a5451831d2b9f691b83619a808947a3c8841606
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 <picard.boris@gmail.com>
2024-03-16 18:46:26 +0100

    about in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,29 +7,29 @@ export default function Home() {
         {/* About */}
         <section
           id="about"
-          className="flex flex-col items-start justify-start gap-4 sm:py-32 py-16"
+          className="flex flex-col items-start justify-start gap-4 sm:py-32 py-16 min-h-screen"
         >
           <About />
         </section>
         {/* Experience */}
         <section
           id="skills"
-          className="flex flex-col items-start justify-start gap-4 sm:py-32 sm:mx-24 md:mx-32 lg:mx-64 xl:mx-96 py-16"
+          className="flex flex-col items-start justify-start gap-4 sm:py-32 sm:mx-24 md:mx-32 lg:mx-64 xl:mx-96 py-16 min-h-screen"
         ></section>
         {/* Projects */}
         <section
           id="projects"
-          className="flex flex-col items-center justify-center gap-4 sm:py-32 py-16"
+          className="flex flex-col items-center justify-center gap-4 sm:py-32 py-16 min-h-screen"
         ></section>
         {/* Skills */}
         <section
           id="skills"
-          className="flex flex-col items-center justify-center gap-4 sm:py-32 py-16"
+          className="flex flex-col items-center justify-center gap-4 sm:py-32 py-16 min-h-screen"
         ></section>
         {/* Contact */}
         <section
           id="contact"
-          className="flex flex-col items-center justify-center gap-4 sm:py-48 py-24"
+          className="flex flex-col items-center justify-center gap-4 sm:py-48 py-24 min-h-screen"
         ></section>
       </div>
     </>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,18 +9,17 @@ export function Hero() {
       id="hero"
       className="min-h-screen relative w-full flex flex-col items-center justify-center overflow-hidden"
     >
-      <div className="lg:container">
+      <div className="md:lg:container">
         <div className="flex flex-col space-y-6">
           <MeteorsDemo />
-          <h1 className="uppercase text-4xl lg:text-7xl text-center lg:text-start font-bold tracking-wide dark:text-white">
+          <h1 className="uppercase text-4xl lg:text-7xl text-center lg:text-start font-black tracking-wide dark:text-white">
             boris picard.
           </h1>
           <span className="uppercase dark:text-white font-bold text-center lg:text-start lg:text-2xl text-xl">
-            Développeur{" "}
-            <span className="text-primary dark:text-[#EF8801]">Web</span>
+            Développeur <span className="text-primary">Web</span>
           </span>
           <div className="flex lg:block md:block">
-            <Button className="hover:shadow-[inset_0_0_0_2px_#616467] px-12 mx-auto py-8 rounded-full tracking-widest uppercase font-bold transition duration-200">
+            <Button className="px-12 mx-auto py-8 rounded-full tracking-widest uppercase font-bold transition duration-200">
               Télécharger mon cv
             </Button>
           </div>
