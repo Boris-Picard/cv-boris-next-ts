0700cfc0ab8909585b78f8da7fe9172b38c6223e
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 <picard.boris@gmail.com>
2024-03-16 12:29:10 +0100

    correction hero et primary color

---
+++ src/components/GlowingCircle.tsx
@@ -0,0 +1,5 @@
+export default function Glowing() {
+  return (
+    <div className="sticky top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] blur-[120px] circlePosition w-[520px] h-[400px] bg-[#F18701] rounded-[100%]"></div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,47 @@
 "use client";
 import React from "react";
 import { FloatingNav } from "./ui/floating-navbar";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import {
+  IconApiApp,
+  IconHome,
+  IconLetterS,
+  IconMessage,
+  IconUser,
+} from "@tabler/icons-react";
 export function FloatingNavDemo() {
   const navItems = [
     {
       name: "Home",
       link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "About",
       link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Skills",
-      link: "/contact",
+      link: "/about",
+      icon: (
+        <IconLetterS className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
     },
     {
       name: "Portfolio",
-      link: "/contact",
+      link: "/about",
+      icon: <IconApiApp className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Contact",
       link: "/contact",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
     },
   ];
   return (
-    <div className="relative  w-full">
+    <div className="relative w-full">
       <FloatingNav navItems={navItems} />
     </div>
   );
