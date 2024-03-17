419e5e6e722be7883e8fc2f1c244b3c84473b6d7
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 <picard.boris@gmail.com>
2024-03-17 18:38:09 +0100

    skills ajouté

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,12 +1,5 @@
 import { Separator } from "@/components/ui/separator";
-import {
-  IconBrandCss3,
-  IconBrandHtml5,
-  IconBrandJavascript,
-  IconBrandNextjs,
-  IconBrandReact,
-} from "@tabler/icons-react";
-import { Button } from "./ui/button";
+import { AnimatedTooltipPreview } from "./Tooltip";
 
 export function SeparatorDemo() {
   return (
@@ -19,40 +12,8 @@ export function SeparatorDemo() {
         </p>
       </div>
       <Separator className="my-4" />
-      <div className="grid-cols-3 grid">
-        <div className="grid-cols-2 grid">
-          <div className="col-span-2 text-center font-medium leading-none text-lg">
-            Front-end
-          </div>
-          <div className="col-span-1 justify-self-center my-4">
-            <IconBrandHtml5 size={50} strokeWidth={1.5} />
-            <IconBrandJavascript size={50} strokeWidth={1.5} />
-            <IconBrandNextjs size={50} strokeWidth={1.5} />
-          </div>
-          {/* <Separator orientation="vertical" /> */}
-          <div className="col-span-1 justify-self-center my-4">
-            <Button
-              className="hover:shadow-[0] cursor-auto"
-              size={"sm"}
-              variant={"link"}
-            >
-              <IconBrandCss3 size={50} strokeWidth={1.5} />
-            </Button>
-            <Button className="hover:shadow-[0]" size={"sm"} variant={"link"}>
-              <IconBrandReact size={50} strokeWidth={1.5} />
-            </Button>
-          </div>
-        </div>
-        <div className="grid-cols-2 grid">
-          <div className="col-span-2 text-center font-medium leading-none text-lg">
-            Back-End
-          </div>
-        </div>
-        <div className="grid grid-cols-2">
-          <div className="col-span-2 text-center font-medium leading-none text-lg">
-            Utils
-          </div>
-        </div>
+      <div className="flex flex-wrap justify-center gap-5">
+        <AnimatedTooltipPreview />
       </div>
     </div>
   );
