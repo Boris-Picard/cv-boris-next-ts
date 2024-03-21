72245bcca6c19170c594edc394b7bf65de70a26c
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 <picard.boris@gmail.com>
2024-03-21 22:43:16 +0100

    section projects done ?

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,7 +8,10 @@ import IconLink from "./Link";
 import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
 
 export default function Projects() {
-  const [mouseOver, setMouseOver] = useState(false);
+  const [isHovered1, setIsHovered1] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const [isHovered3, setIsHovered3] = useState(false);
+  const [isHovered4, setIsHovered4] = useState(false);
 
   return (
     <>
@@ -20,15 +23,15 @@ export default function Projects() {
       </div>
       <Card className="flex flex-col lg:flex-row">
         <div className="lg:w-1/2 flex flex-wrap">
-          <CardHeader>
+          <CardHeader className="my-3 xl:lg:md:my-0">
             <div className="flex items-center">
               <span className="text-primary font-bold text-xl">01.</span>
-              <h3 className="uppercase lg:md:text-2xl text-xl lg:mx-3 lg:text-start text-center font-extrabold tracking-wide dark:text-white">
+              <h3 className="uppercase lg:md:text-2xl text-xl mx-3 lg:text-start text-center font-extrabold tracking-wide dark:text-white">
                 Blazerifle
               </h3>
             </div>
           </CardHeader>
-          <CardContent className="p-0">
+          <CardContent className="p-0 mb-3 xl:lg:md:mb-0">
             <p className="text-base leading-relaxed font-medium">
               Projet pour l'obtention du titre RNCP Niveau 5 "Développeur Web et
               Web Mobile" : Maquette Figma, MERISE, MySQL, PHP. Gestion complète
@@ -40,7 +43,7 @@ export default function Projects() {
             </p>
           </CardContent>
           <CardFooter className="flex justify-between">
-            <div className="flex flex-wrap space-x-3">
+            <div className="flex flex-wrap gap-3 my-3">
               <Badge variant="secondary">HTML</Badge>
               <Badge variant="secondary">CSS</Badge>
               <Badge variant="secondary">Javascript</Badge>
@@ -48,20 +51,26 @@ export default function Projects() {
               <Badge variant="secondary">MySql</Badge>
               <Badge variant="secondary">POO</Badge>
             </div>
-            <div className="flex justify-around space-x-3">
-              <IconLink href="/" icon="Link">
+            <div className="flex justify-center lg:justify-start mt-3 lg:mt-0 space-x-3">
+              <IconLink
+                href="https://blazerifle.boris-picard.fr/controllers/home-ctrl.php"
+                icon="Link"
+              >
                 Voir le site
               </IconLink>
-              <IconLink href="/" icon="Github">
+              <IconLink
+                href="https://github.com/Boris-Picard/BlazeRifle"
+                icon="Github"
+              >
                 Code
               </IconLink>
             </div>
           </CardFooter>
         </div>
         <div
-          className="lg:w-1/2 overflow-hidden rounded-3xl"
-          onMouseEnter={() => setMouseOver(true)}
-          onMouseLeave={() => setMouseOver(false)}
+          className="lg:w-1/2 overflow-hidden mt-6 lg:mt-0 rounded-3xl flex"
+          onMouseEnter={() => setIsHovered1(true)}
+          onMouseLeave={() => setIsHovered1(false)}
         >
           <AspectRatio ratio={16 / 9}>
             <Image
@@ -70,17 +79,55 @@ export default function Projects() {
               quality={75}
               alt="blazerifle website"
               className={`object-cover rounded-3xl transition-all duration-500  ${
-                mouseOver ? "scale-110" : ""
+                isHovered1 ? "scale-110" : ""
               }`}
             />
           </AspectRatio>
         </div>
       </Card>
-      <Card className="flex flex-col lg:flex-row text-end my-4">
+
+      <Card className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/2 flex flex-wrap">
+          <CardHeader className="my-3 xl:lg:md:my-0">
+            <div className="flex items-center">
+              <span className="text-primary font-bold text-xl">02.</span>
+              <h3 className="uppercase lg:md:text-2xl text-xl mx-3 lg:text-start text-center font-extrabold tracking-wide dark:text-white">
+                Rent my ride
+              </h3>
+            </div>
+          </CardHeader>
+          <CardContent className="p-0 mb-3 xl:lg:md:mb-0">
+            <p className="text-base leading-relaxed font-medium">
+              Premier projet MERISE, MySQL, PHP : Site location voiture avec
+              CRUD complet, tableau de bord, barre de recherche. Sécurité
+              renforcée : nettoyage et validation des données côté serveur,
+              PHPMailer pour confirmation location. Expérience utilisateur
+              fluide et sécurisée.
+            </p>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <div className="flex flex-wrap gap-3 my-3">
+              <Badge variant="secondary">HTML</Badge>
+              <Badge variant="secondary">CSS</Badge>
+              <Badge variant="secondary">Javascript</Badge>
+              <Badge variant="secondary">PHP</Badge>
+              <Badge variant="secondary">MySql</Badge>
+              <Badge variant="secondary">POO</Badge>
+            </div>
+            <div className="flex justify-center lg:justify-start mt-3 lg:mt-0 space-x-3">
+              <IconLink href="" icon="Link">
+                Voir le site
+              </IconLink>
+              <IconLink href="" icon="Github">
+                Code
+              </IconLink>
+            </div>
+          </CardFooter>
+        </div>
         <div
-          className="lg:w-1/2 overflow-hidden rounded-3xl"
-          onMouseEnter={() => setMouseOver(true)}
-          onMouseLeave={() => setMouseOver(false)}
+          className="lg:w-1/2 overflow-hidden mt-6 lg:mt-0 rounded-3xl flex"
+          onMouseEnter={() => setIsHovered2(true)}
+          onMouseLeave={() => setIsHovered2(false)}
         >
           <AspectRatio ratio={16 / 9}>
             <Image
@@ -89,43 +136,102 @@ export default function Projects() {
               quality={75}
               alt="rent my ride website"
               className={`object-cover rounded-3xl transition-all duration-500  ${
-                mouseOver ? "scale-110" : ""
+                isHovered2 ? "scale-110" : ""
               }`}
             />
           </AspectRatio>
         </div>
-        <div className="lg:w-1/2 flex flex-wrap justify-end">
-          <CardHeader>
+      </Card>
+
+      <Card className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/2 flex flex-wrap">
+          <CardHeader className="my-3 xl:lg:md:my-0">
             <div className="flex items-center">
-              <span className="text-primary font-bold text-end text-xl">
-                02.
-              </span>
-              <h3 className="uppercase lg:md:text-2xl text-end text-xl lg:mx-3 font-extrabold tracking-wide dark:text-white">
-                Rent my ride
+              <span className="text-primary font-bold text-xl">03.</span>
+              <h3 className="uppercase lg:md:text-2xl text-xl mx-3 lg:text-start text-center font-extrabold tracking-wide dark:text-white">
+                reacttasks
               </h3>
             </div>
           </CardHeader>
-          <CardContent className="p-0">
+          <CardContent className="p-0 mb-3 xl:lg:md:mb-0">
             <p className="text-base leading-relaxed font-medium">
-              Projet pour l'obtention du titre RNCP Niveau 5 "Développeur Web et
-              Web Mobile" : Maquette Figma, MERISE, MySQL, PHP. Gestion complète
-              des utilisateurs, articles, commentaires, favoris. Sécurité
-              renforcée : validation d'email (JWT), hashage des mots de passe,
-              RGPD. Utilisation de singleton et transactions pour la sécurité
-              des opérations sur la base de données. SEO optimisé, respect des
-              normes W3C. Mise en ligne sur Hostinger.
+              Projet réalisé avec React dans le cadre de mon apprentissage
+              autonome afin d'acquérir les bases essentielles, telles que JSX,
+              les composants (fonctionnels et de classe), la distinction entre
+              les props et l'état, la gestion des événements, les listes et les
+              clés, les composants de haute ordre, ainsi que les hooks
+              (useState, useEffect, etc.). Utilisation de Tailwind CSS pour le
+              stylisme et intégration d'une bibliothèque de composants UI avec
+              Next UI.
             </p>
           </CardContent>
           <CardFooter className="flex justify-between">
-            <div className="flex flex-wrap space-x-3 justify-end">
-              <Badge variant="secondary">HTML</Badge>
-              <Badge variant="secondary">CSS</Badge>
-              <Badge variant="secondary">Javascript</Badge>
-              <Badge variant="secondary">PHP</Badge>
-              <Badge variant="secondary">MySql</Badge>
-              <Badge variant="secondary">POO</Badge>
+            <div className="flex flex-wrap gap-3 my-3">
+              <Badge variant="secondary">React</Badge>
+              <Badge variant="secondary">Tailwind</Badge>
+              <Badge variant="secondary">NextUI</Badge>
             </div>
-            <div className="flex justify-around space-x-3">
+            <div className="flex justify-center lg:justify-start mt-3 lg:mt-0 space-x-3">
+              <IconLink
+                href="https://boris-picard.github.io/todo-list-react/"
+                icon="Link"
+              >
+                Voir le site
+              </IconLink>
+              <IconLink
+                href="https://github.com/Boris-Picard/todo-list-react"
+                icon="Github"
+              >
+                Code
+              </IconLink>
+            </div>
+          </CardFooter>
+        </div>
+        <div
+          className="lg:w-1/2 overflow-hidden mt-6 lg:mt-0 rounded-3xl flex"
+          onMouseEnter={() => setIsHovered3(true)}
+          onMouseLeave={() => setIsHovered3(false)}
+        >
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              src={"/img/reacttasks.png"}
+              fill={true}
+              quality={75}
+              alt="reacttasks website"
+              className={`object-cover rounded-3xl transition-all duration-500  ${
+                isHovered3 ? "scale-110" : ""
+              }`}
+            />
+          </AspectRatio>
+        </div>
+      </Card>
+
+      <Card className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/2 flex flex-wrap">
+          <CardHeader className="my-3 xl:lg:md:my-0">
+            <div className="flex items-center">
+              <span className="text-primary font-bold text-xl">04.</span>
+              <h3 className="uppercase lg:md:text-2xl text-xl mx-3 lg:text-start text-center font-extrabold tracking-wide dark:text-white">
+                cv numerique
+              </h3>
+            </div>
+          </CardHeader>
+          <CardContent className="p-0 mb-3 xl:lg:md:mb-0">
+            <p className="text-base leading-relaxed font-medium">
+              Création d'un CV numérique avec Next.js pour explorer TypeScript.
+              Expérience approfondie avec les bibliothèques d'UI, notamment
+              Shadcn et Aceternity, combinées avec Tailwind CSS.
+            </p>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <div className="flex flex-wrap gap-3 my-3">
+              <Badge variant="secondary">NextJs</Badge>
+              <Badge variant="secondary">Typescript</Badge>
+              <Badge variant="secondary">Tailwind</Badge>
+              <Badge variant="secondary">Shadcn</Badge>
+              <Badge variant="secondary">Aceternity</Badge>
+            </div>
+            <div className="flex justify-center lg:justify-start mt-3 lg:mt-0 space-x-3">
               <IconLink href="/" icon="Link">
                 Voir le site
               </IconLink>
@@ -134,6 +240,23 @@ export default function Projects() {
               </IconLink>
             </div>
           </CardFooter>
+        </div>
+        <div
+          className="lg:w-1/2 overflow-hidden mt-6 lg:mt-0 rounded-3xl flex"
+          onMouseEnter={() => setIsHovered4(true)}
+          onMouseLeave={() => setIsHovered4(false)}
+        >
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              src={"/img/cv-numerique.png"}
+              fill={true}
+              quality={75}
+              alt="cv numérique website"
+              className={`object-cover rounded-3xl transition-all duration-500  ${
+                isHovered4 ? "scale-110" : ""
+              }`}
+            />
+          </AspectRatio>
         </div>
       </Card>
     </>
